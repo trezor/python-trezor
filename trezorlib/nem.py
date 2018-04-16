194b534eb94e9a3ab3d26c6fbe84ab89910e0be7
matejcik <ja@matejcik.cz>
2018-04-16 13:49:06 +0200

    nem: converted to ProtoAdapter

--- trezorlib/nem.py
+++ trezorlib/nem.py
@@ -1,5 +1,7 @@
 import binascii
 import json
+
+from trezorlib import mapping
 from . import messages as proto
 
 TYPE_TRANSACTION_TRANSFER = 0x0101
@@ -12,138 +14,125 @@ TYPE_MOSAIC_CREATION = 0x4001
 TYPE_MOSAIC_SUPPLY_CHANGE = 0x4002
 
 
-def create_transaction_common(transaction):
-    msg = proto.NEMTransactionCommon()
-    msg.network = (transaction["version"] >> 24) & 0xFF
-    msg.timestamp = transaction["timeStamp"]
-    msg.fee = transaction["fee"]
-    msg.deadline = transaction["deadline"]
-
-    if "signer" in transaction:
-        msg.signer = binascii.unhexlify(transaction["signer"])
-
-    return msg
+adapter_nem_transaction_common = mapping.ProtoAdapter(
+    proto.NEMTransactionCommon,
+    {
+        'network': lambda t: (t['version'] >> 24) & 0xff,
+        'timestamp': 'timeStamp',
+    },
+)
 
 
-def create_transfer(transaction):
-    msg = proto.NEMTransfer()
-    msg.recipient = transaction["recipient"]
-    msg.amount = transaction["amount"]
-
-    if "payload" in transaction["message"]:
-        msg.payload = binascii.unhexlify(transaction["message"]["payload"])
-
-        if transaction["message"]["type"] == 0x02:
-            msg.public_key = binascii.unhexlify(transaction["message"]["publicKey"])
-
-    if "mosaics" in transaction:
-        msg.mosaics = [proto.NEMMosaic(
-            namespace=mosaic["mosaicId"]["namespaceId"],
-            mosaic=mosaic["mosaicId"]["name"],
-            quantity=mosaic["quantity"],
-        ) for mosaic in transaction["mosaics"]]
-
-    return msg
+adapter_nem_transfer = mapping.ProtoAdapter(
+    proto.NEMTransfer,
+    {
+        'payload': 'message.payload',
+        'public_key': 'message.publicKey',
+        'mosaics': [{
+            'namespace': 'mosaicId.namespaceId',
+            'mosaic': 'mosaicId.name'
+        }],
+    },
+)
 
 
-def create_aggregate_modification(transactions):
-    msg = proto.NEMAggregateModification()
-    msg.modifications = [proto.NEMCosignatoryModification(
-        type=modification["modificationType"],
-        public_key=binascii.unhexlify(modification["cosignatoryAccount"]),
-    ) for modification in transactions["modifications"]]
-
-    if "minCosignatories" in transactions:
-        msg.relative_change = transactions["minCosignatories"]["relativeChange"]
-
-    return msg
-
-
-def create_provision_namespace(transaction):
-    msg = proto.NEMProvisionNamespace()
-    msg.namespace = transaction["newPart"]
-
-    if transaction["parent"]:
-        msg.parent = transaction["parent"]
-
-    msg.sink = transaction["rentalFeeSink"]
-    msg.fee = transaction["rentalFee"]
-    return msg
+adapter_nem_aggregate_modification = mapping.ProtoAdapter(
+    proto.NEMAggregateModification,
+    {
+        'modifications': [{
+            'type': 'modificationType',
+            'public_key': 'cosignatoryAccount',
+        }],
+        'relative_change': 'minCosignatories.relativeChange',
+    }
+)
 
 
-def create_mosaic_creation(transaction):
-    definition = transaction["mosaicDefinition"]
-    msg = proto.NEMMosaicCreation()
-    msg.definition = proto.NEMMosaicDefinition()
-    msg.definition.namespace = definition["id"]["namespaceId"]
-    msg.definition.mosaic = definition["id"]["name"]
-
-    if definition["levy"]:
-        msg.definition.levy = definition["levy"]["type"]
-        msg.definition.fee = definition["levy"]["fee"]
-        msg.definition.levy_address = definition["levy"]["recipient"]
-        msg.definition.levy_namespace = definition["levy"]["mosaicId"]["namespaceId"]
-        msg.definition.levy_mosaic = definition["levy"]["mosaicId"]["name"]
-
-    msg.definition.description = definition["description"]
-
-    for property in definition["properties"]:
-        name = property["name"]
-        value = json.loads(property["value"])
-
-        if name == "divisibility":
-            msg.definition.divisibility = value
-        elif name == "initialSupply":
-            msg.definition.supply = value
-        elif name == "supplyMutable":
-            msg.definition.mutable_supply = value
-        elif name == "transferable":
-            msg.definition.transferable = value
-
-    msg.sink = transaction["creationFeeSink"]
-    msg.fee = transaction["creationFee"]
-    return msg
+adapter_nem_provision_namespace = mapping.ProtoAdapter(
+    proto.NEMProvisionNamespace,
+    {
+        'namespace': 'newPart',
+        'sink': 'rentalFeeSink',
+        'fee': 'rentalFee',
+    },
+)
 
 
-def create_supply_change(transaction):
-    msg = proto.NEMMosaicSupplyChange()
-    msg.namespace = transaction["mosaicId"]["namespaceId"]
-    msg.mosaic = transaction["mosaicId"]["name"]
-    msg.type = transaction["supplyType"]
-    msg.delta = transaction["delta"]
-    return msg
+adapter_modified_mosaic_definition = mapping.ProtoAdapter(
+    proto.NEMMosaicDefinition,
+    {
+        'namespace': 'id.namespaceId',
+        'mosaic': 'id.name',
+        'levy': 'levy.type',
+        'fee': 'levy.fee',
+        'levy_address': 'levy.recipient',
+        'levy_namespace': 'levy.mosaicId.namespaceId',
+        'levy_mosaic': 'levy.mosaicId.name',
+        'divisibility': 'properties.divisibility',
+        'supply': 'properties.initialSupply',
+        'mutable_supply': 'properties.supplyMutable',
+        'transferable': 'properties.transferable',
+    },
+)
 
 
-def create_importance_transfer(transaction):
-    msg = proto.NEMImportanceTransfer()
-    msg.mode = transaction["importanceTransfer"]["mode"]
-    msg.public_key = binascii.unhexlify(transaction["importanceTransfer"]["publicKey"])
-    return msg
+def adapter_nem_mosaic_definition(transaction):
+    definition = transaction['mosaicDefinition'].copy()
+    properties = definition['properties']
+    definition['properties'] = {prop['name']: json.loads(prop['value']) for prop in properties}
+    return adapter_modified_mosaic_definition(definition)
+
+
+adapter_nem_mosaic_creation = mapping.ProtoAdapter(
+    proto.NEMMosaicCreation,
+    {
+        'sink': 'creationFeeSink',
+        'fee': 'creationFee',
+        'definition': adapter_nem_mosaic_definition,
+    }
+)
+
+
+adapter_nem_supply_change = mapping.ProtoAdapter(
+    proto.NEMMosaicSupplyChange,
+    {
+        'namespace': 'mosaicId.namespaceId',
+        'mosaic': 'mosaicId.name',
+        'type': 'supplyType'
+    },
+)
+
+
+adapter_nem_importance_transfer = mapping.ProtoAdapter(
+    proto.NEMImportanceTransfer,
+    select_field='importanceTransfer',
+)
 
 
 def create_sign_tx(transaction):
     msg = proto.NEMSignTx()
-    msg.transaction = create_transaction_common(transaction)
+    msg.transaction = adapter_nem_transaction_common(transaction)
     msg.cosigning = (transaction["type"] == TYPE_MULTISIG_SIGNATURE)
 
     if transaction["type"] in (TYPE_MULTISIG_SIGNATURE, TYPE_MULTISIG):
         transaction = transaction["otherTrans"]
-        msg.multisig = create_transaction_common(transaction)
+        msg.multisig = adapter_nem_transaction_common(transaction)
     elif "otherTrans" in transaction:
         raise ValueError("Transaction does not support inner transaction")
 
-    if transaction["type"] == TYPE_TRANSACTION_TRANSFER:
-        msg.transfer = create_transfer(transaction)
-    elif transaction["type"] == TYPE_AGGREGATE_MODIFICATION:
-        msg.aggregate_modification = create_aggregate_modification(transaction)
-    elif transaction["type"] == TYPE_PROVISION_NAMESPACE:
-        msg.provision_namespace = create_provision_namespace(transaction)
-    elif transaction["type"] == TYPE_MOSAIC_CREATION:
-        msg.mosaic_creation = create_mosaic_creation(transaction)
-    elif transaction["type"] == TYPE_MOSAIC_SUPPLY_CHANGE:
-        msg.supply_change = create_supply_change(transaction)
-    elif transaction["type"] == TYPE_IMPORTANCE_TRANSFER:
-        msg.importance_transfer = create_importance_transfer(transaction)
+    tx_type = transaction["type"]
+    if tx_type == TYPE_TRANSACTION_TRANSFER:
+        msg.transfer = adapter_nem_transfer(transaction)
+    elif tx_type == TYPE_AGGREGATE_MODIFICATION:
+        msg.aggregate_modification = adapter_nem_aggregate_modification(transaction)
+    elif tx_type == TYPE_PROVISION_NAMESPACE:
+        msg.provision_namespace = adapter_nem_provision_namespace(transaction)
+    elif tx_type == TYPE_MOSAIC_CREATION:
+        msg.mosaic_creation = adapter_nem_mosaic_creation(transaction)
+    elif tx_type == TYPE_MOSAIC_SUPPLY_CHANGE:
+        msg.supply_change = adapter_nem_supply_change(transaction)
+    elif tx_type == TYPE_IMPORTANCE_TRANSFER:
+        msg.importance_transfer = adapter_nem_importance_transfer(transaction)
     else:
         raise ValueError("Unknown transaction type")
 
